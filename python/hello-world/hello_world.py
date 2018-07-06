e5c93afde355579b8250e6515c1577304449e806
Guter Stiel <opi70@web.de>
2018-07-06 12:25:40 +0200

    leap year läuft

--- python/hello-world/hello_world.py
+++ python/hello-world/hello_world.py
@@ -1,3 +1,11 @@
+import time
 print("hello world")
+
 def hello(name=''):
-    pass
+	print("Nice to meet you " + name + "!")
+
+name = input("What's your name? ")
+hello(name)
+age = input("Your age? ")
+print("So, you are already " + str(age) + " years old, " + name + "!")
+
--- python/leap/leap.py
+++ python/leap/leap.py
@@ -1,2 +1,5 @@
 def is_leap_year(year):
-    pass
+	if year % 400 == 0 or (year % 4 == 0 and not year % 100 == 0):
+		return True
+	else:
+		return False
